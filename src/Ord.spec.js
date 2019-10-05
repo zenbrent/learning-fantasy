72f771809b4c111b13a682de2bb77b5a411ba885
Brent Brimhall <brent.brimhall@gmail.com>
2019-10-04 19:28:59 -0500

    Starting Ord

---
+++ src/Ord.spec.js
@@ -0,0 +1,18 @@
+/**
+ * Setoid: equivalence
+ * equals :: Setoid a => a ~> a -> Boolean
+ */
+
+import { tagged } from 'daggy';
+
+import { List } from './List';
+import { Bool } from './Bool';
+import { Set_, nub } from './Set';
+
+import { Coord, Line, Shape } from './Shapes';
+
+describe('Ord', () => {
+  test('List', () => {
+  });
+});
+
