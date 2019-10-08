f5887881645310d43cc27255ae20087b2fcc60b3
Brent Brimhall <brent.brimhall@gmail.com>
2019-10-08 01:15:23 -0500

    Starting functor

---
+++ src/Functor.spec.js
@@ -0,0 +1,37 @@
+/**
+ * Functor
+ * map :: Functor f => f a ~> (a -> b) -> f b
+ *
+ * Laws:
+ * Identity:
+ * u.map(x => x) == u
+ *
+ * Composition:
+ * u.map(f).map(g) == u.map(x => f(g(x)))
+ *
+ * Intuition:
+ * Sets aren't functors -- their inner type must
+ * be a semigroup, and functors don't restrict
+ * the contents to be semigroups.
+ *
+ * http://www.tomharding.me/2017/03/27/fantas-eel-and-specification-6/
+ */
+
+import { Customer } from './Customer';
+import { Tuple, TupleMonoid } from './Tuple';
+import { List } from './List';
+import { Bool } from './Bool';
+import { patchBuiltins } from './Number';
+import { Coord, Line, Shape } from './Shapes';
+import {
+  foldL, foldR,
+  Sum, Product, Any, All, First, Last, Min, Max
+} from './Semigroup';
+import { foldLP } from './ParallelFold';
+
+patchBuiltins();
+
+describe('Functor', () => {
+  test.skip('', () => {
+  });
+});
