821d7b754eb5ed93ec91ee492c8dd24f66244048
Brent Brimhall <brent.brimhall@gmail.com>
2019-10-23 19:46:53 -0500

    Starting traversable

---
+++ src/Traversable.spec.js
@@ -0,0 +1,23 @@
+/***********************************
+ * Traversable
+ *
+ * Laws:
+ *
+ * Intuition:
+ *
+ *
+ * http://www.tomharding.me/2017/05/08/fantas-eel-and-specification-12/
+ */
+
+
+import { Just, Nothing } from './Maybe';
+import { Left, Right } from './Either';
+import { BTree } from './BTree';
+
+import { patchArray } from './Array';
+patchArray();
+
+describe('Traversable', () => {
+  test.skip('Maybe', () => {
+  });
+});
